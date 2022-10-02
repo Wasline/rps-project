47d83612818e69c37f729ed637755845ebe69f5c
wasline <sf.wasline@gmail.com>
2022-10-02 13:19:24 -0400

    created the app
    with react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Game from "./Game";
+
+
+export default function App() { return (
+    <div className="App">
+      <div className="container">
+        <Header/>
+            <Game />
+            </div>
+            </div>
+  );
+}
+ 
