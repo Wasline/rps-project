37127234bff9fa6ba1292aa09c5055d6c16e5082
wasline <sf.wasline@gmail.com>
2022-10-02 14:02:01 -0400

    Create Footer.js

---
+++ src/component/Footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Footer(){
+return (
+  <div className="footer">
+    <footer className="text-center">
+      Coded by Wasline Saint fleur and{" "}
+      <a
+        href="https://github.com/isabelklint"
+        className="text-decoration-none"
+        target="_blank"
+        rel="noreferrer"
+        title="link to github"
+      >
+        Isabel Klint
+      </a>
+      it's{" "}
+      <a
+        href="https://github.com/Wasline/rps-project"
+        className="text-decoration-none"
+        target="_blank"
+        rel="noreferrer"
+        title="link to github"
+      >
+        open-sourced
+      </a>
+    </footer>
+  </div>
+);
+};
+
